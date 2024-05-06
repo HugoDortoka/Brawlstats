2ca401af80fe3a071a4d42614e065c6349a5b607
HugoDortoka <hugo.varela@inslapineda.cat>
2024-05-06 20:03:32 +0200

    HEader

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom"
 
-// Layout.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../CSS/main.css';
 
 function Layout() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
       <header className="header">
@@ -13,19 +18,27 @@ function Layout() {
           <h1>Brawl Stats</h1>
         </div>
         <div className="middle">
+          {/* Enlaces del menú */}
           <Link to="/" className="link">Search</Link>
           <Link to="/wiki" className="link">Wiki</Link>
           <Link to="/top" className="link">Top</Link>
         </div>
-        <div className="right">
-          <Link to="/register" className="link">Register</Link>
-          <span>|</span>
-          <Link to="/login" className="link">Login</Link>
+        <div className={`right ${menuOpen ? 'open' : ''}`}>
+          {/* Botón de menú para dispositivos móviles */}
+          <button className="menu-button" onClick={toggleMenu}>
+            Menu
+          </button>
+          {/* Enlaces de registro y inicio de sesión */}
+          <div>
+            <Link to="/register" className="link">Register</Link>
+            <span className="link"> | </span>
+            <Link to="/login" className="link">Login</Link>
+          </div>
         </div>
       </header>
+      {/* Outlet para mostrar el contenido */}
       <Outlet />
     </div>
-    
   );
 }
 
