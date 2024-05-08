6c079094e833483bb1b9ba20922e4c24c90b3839
HugoDortoka <hugo.varela@inslapineda.cat>
2024-05-08 19:50:54 +0200

    Footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+// Footer.js
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Este es el pie de página de mi aplicación.</p>
+    </footer>
+  );
+}
+
+export default Footer;
