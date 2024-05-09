324cc8fb6f633c18dee783699f35e3a7f196e2d8
HugoDortoka <hugo.varela@inslapineda.cat>
2024-05-10 01:04:47 +0200

    Main todo responsive CON IMAGENES, FALTA CAMBIA TIPOGRAFIA

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom';
 import '../CSS/main.css';
 
 function Layout({ userLogged }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     const toggleBtn = document.querySelector('.toggle_btn');
     const dropDownMenu = document.querySelector('.dropdown_menu');
 
     const toggleMenu = () => {
-      dropDownMenu.classList.toggle('open');
-      const isOpen = dropDownMenu.classList.contains('open');
-      toggleBtn.textContent = isOpen ? 'CERRAR' : 'MENU';
+      setIsOpen(!isOpen);
     };
 
     toggleBtn.addEventListener('click', toggleMenu);
 
     // Limpia el evento al desmontar el componente
     return () => toggleBtn.removeEventListener('click', toggleMenu);
-  }, []); // Asegura que se ejecute solo una vez después del montaje inicial
+  }, [isOpen]);
 
   return (
     <div>
@@ -37,10 +37,14 @@ function Layout({ userLogged }) {
             <Link to="/login" className="action_btn">Log In</Link>
           )}
           <div className="toggle_btn">
-            MENU
+            {isOpen ? (
+              <img className="menu" src="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='25' height='25' viewBox='0 0 24 24'%3e%3cline x1='1' y1='1' x2='23' y2='23' stroke='%23000' stroke-width='2'/%3e%3cline x1='23' y1='1' x2='1' y2='23' stroke='%23000' stroke-width='2'/%3e%3c/svg%3e"></img>
+            ) : (
+              <img className="menu" src="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='%23000' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e"></img>
+            )}
           </div>
         </div>
-        <div className="dropdown_menu">
+        <div className={`dropdown_menu ${isOpen ? 'open' : ''}`}>
           <li><Link to="/" className="link">Search</Link></li>
           <li><Link to="/wiki" className="link">Wiki</Link></li>
           <li><Link to="/top" className="link">Top</Link></li>
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -38,14 +38,17 @@ function Search() {
     return (
         <div className="container">
             <div className="form">
-                <input
-                    type="text"
-                    className="form__input"
-                    placeholder="#XXXXXXXX"
-                    value={playerTag}
-                    onChange={(e) => setPlayerTag(e.target.value)}
-                />
-                <button className="form__submit" onClick={getPlayerDetails}>Get Details</button>
+                <div>Enter your player tag to track own stats</div>
+                <div className='searchBar'>
+                    <input
+                        type="text"
+                        className="form__input"
+                        placeholder="#XXXXXXXX"
+                        value={playerTag}
+                        onChange={(e) => setPlayerTag(e.target.value)}
+                    />
+                    <button className="form__submit" onClick={getPlayerDetails}><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='40' height='40'%3E%3Cpath fill='%23fff' d='M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3C/svg%3E" alt="Search Icon"></img></button>
+                </div>
             </div>
 
             {error && <div className="error">{error}</div>}
@@ -112,7 +115,7 @@ function Search() {
                             </div>
                         </div>
                     </div>
-                    <h1>Brawlers ({playerData.brawlers.length}/{brawlersData.items.length})</h1>
+                    <h1 className='titleBrawlers'>Brawlers ({playerData.brawlers.length}/{brawlersData.items.length})</h1>
                     <div className='brawlersContainer'>
                         {playerData.brawlers.map(brawler => (
                             <BrawlContainer key={brawler.id} brawler={brawler} />
