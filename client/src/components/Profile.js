626e15b631ed2a145074a296b0c96f8ff6e0fa54
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2024-05-14 11:50:42 +0200

    battle log colors

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -54,7 +54,7 @@ function Profile({ onUserLogout }) {
     
     useEffect(() => {
 
-        if (battleLog) {
+        if (isDataFetched && battleLog!==null) {
             battleLog.items.forEach((item, index) => {
                 var resultadoDiv = document.getElementById(`result${index}`);
                 if (resultadoDiv!=null) {
@@ -66,7 +66,7 @@ function Profile({ onUserLogout }) {
                 };
             });
         }
-    }, [battleLog]);
+    }, [battleLog, isDataFetched]);
 
 
     return (
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -58,7 +58,7 @@ function Search() {
     }
     
     useEffect(() => {
-        if (battleLog) {
+        if (isDataFetched && battleLog!==null) {
             battleLog.items.forEach((item, index) => {
                 var resultadoDiv = document.getElementById(`result${index}`);
                 if (resultadoDiv != null) {
@@ -70,7 +70,7 @@ function Search() {
                 }
             });
         }
-    }, [battleLog]);
+    }, [battleLog, isDataFetched]);
     
     return (
         <div className="container">
@@ -87,8 +87,6 @@ function Search() {
                     <button className="form__submit" onClick={getPlayerDetails}><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='40' height='40'%3E%3Cpath fill='%23fff' d='M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3C/svg%3E" alt="Search Icon"></img></button>
                 </div>
             </div>
-
-            {error && <div className="error">{error}</div>}
 
             <div ref={detailsRef}></div>
             {isDataFetched && (
